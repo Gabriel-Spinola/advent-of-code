d9bc354baf52cd4c08c1d13f927251eb8d9ccc5f
Gabriel-Spinola <gabrielspinola77@gmail.com>
2024-12-05 16:35:09 -0300

    feat: Day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,35 @@
+use std::{collections::btree_map::Values, fs, ops::Sub};
+
+/**
+ * find 50 stars
+ */
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string(
+        "C:/Users/Gabriel Spinola/Desktop/projects/AdventOfCode/day1/src/input.txt",
+    )
+    .expect("Shoud have benn able to read the file");
+
+    let listed = input.split_ascii_whitespace();
+    let mut left: Vec<i32> = listed
+        .clone()
+        .enumerate()
+        .filter(|&(i, _)| i % 2 == 0)
+        .map(|(_, value)| value.parse::<i32>().expect("Failed to parte value"))
+        .collect();
+    let mut right: Vec<i32> = listed
+        .enumerate()
+        .filter(|&(i, _)| i % 2 != 0)
+        .map(|(_, value)| value.parse::<i32>().expect("Failed to parte value"))
+        .collect();
+
+    left.sort();
+    right.sort();
+    let mut right_iter = right.iter_mut();
+
+    let distances: i32 = left
+        .into_iter()
+        .map(|value| (right_iter.next().unwrap().sub(value)).abs())
+        .sum();
+
+    print!("{}", distances)
 }
